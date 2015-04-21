15452ca4118c5715b1f273bcd0f29b12ce03522c
Danya Lagos <tollibaa@gmail.com>
2015-04-21 13:52:50 -0500

    need to define name but keep looking in fetchArtist.py in Assignment 5.

--- Assignment6/is.py
+++ Assignment6/is.py
@@ -17,6 +17,27 @@ def getRelatedArtists(artistID):
 
 	return relatedartist_List
 
-print getRelatedArtists("6deZN1bslXzeGvOLaLMOIF")
+#print getRelatedArtists("6deZN1bslXzeGvOLaLMOIF")
 
 #NEXT STEPS - INCORPORATE THIS SOMEHOW INTO FUNCTION THAT REPLICATES THE FUNCTION OF artistNetworks.py
+
+def getDepthEdges(artistID, depth):
+    
+    directedPairs = []
+    directedPairs_checked = []
+    relatedArtists = []
+    relatedArtists.append(name)
+
+    for i in range(depth):
+    	for relatedartist in relatedArtists: 
+    		newartists = getRelatedArtists(relatedartist)
+    		for newartist in newartists:
+    			directedPairs.append((relatedartist, newartist))
+    		relatedArtists = newartists
+    	for tple in directedPairs:
+    		if tple not in directedPairs_checked:
+    			directedPairs_checked.append(tple)
+
+    	return directedPairs_checked
+
+print getDepthEdges("6deZN1bslXzeGvOLaLMOIF", 2)
